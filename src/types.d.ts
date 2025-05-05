aae9a678cea9d9ac469f846ad3502005b0719cf8
Harikrishnan V K <85347203+hk-vk@users.noreply.github.com>
2025-05-06 01:28:20 +0530

    feat: Add type declarations for meshLineGeometry and meshLineMaterial

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,11 @@
 declare module "*.glb" {
   const src: string;
   export default src;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    meshLineGeometry: any;
+    meshLineMaterial: any;
+  }
 } 
